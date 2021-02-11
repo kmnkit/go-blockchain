bc1012e26f08fc1d3a5db8e941674855677cc2c8
Marco Kang <kmnkit@gmail.com>
2021-02-11 18:50:00 +0900

    4. Slice

--- github.com/kmnkit/learngo/main.go
+++ github.com/kmnkit/learngo/main.go
@@ -3,17 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	a := 2  // a의 값이 2
-	b := &a // a의 메모리 주소
-
-	fmt.Println(&a, b) // a의 메모리 주소와 b의 값(a의 메모리 주소)를 출력
-	fmt.Println(*b)    // b에 있는 실제 값을 출력
-
-	fmt.Println(&a) // a의 메모리주소
-	fmt.Println(a)  // a에 있는 값
-	fmt.Println(&b) // b의 메모리주소
-	fmt.Println(b)  // b의 값
-	*b = 20
-	fmt.Println(a) // a의 값이 20으로 변경되었음
-	fmt.Println(b) // b의 값(a의 주소값)은 그대로
+	// names := [5]string{"nico", "lynn", "dal"}
+	// names[3] = "alalala"
+	// names[4] = "ahahaha"
+	// index를 넘어가므로 아래 코드는 에러가 남.
+	// names[5] = "jajaja"
+	names := []string{"nico", "lynn", "dal"}
+	names = append(names, "flynn")
+	fmt.Println(names)
 }
