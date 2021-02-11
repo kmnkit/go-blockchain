15de9914201609f98484fbb0e78ba85abd09b34a
Marco Kang <kmnkit@gmail.com>
2021-02-11 18:55:35 +0900

    5.map

--- github.com/kmnkit/learngo/main.go
+++ github.com/kmnkit/learngo/main.go
@@ -3,12 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	// names := [5]string{"nico", "lynn", "dal"}
-	// names[3] = "alalala"
-	// names[4] = "ahahaha"
-	// index를 넘어가므로 아래 코드는 에러가 남.
-	// names[5] = "jajaja"
-	names := []string{"nico", "lynn", "dal"}
-	names = append(names, "flynn")
-	fmt.Println(names)
+	nico := map[string]string{"name": "nico", "age": "12"}
+	for key, _ := range nico {
+		fmt.Println(key)
+	}
 }
